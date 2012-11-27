57609784e8b576cd5d19bc5762a20ab666faa773
Lech Stawikowski <lech.stawikowski@gmail.com>
2012-11-27 20:00:38 +0100

    pierwsza wersja glownego layoutu
    Glowne okno podzielone jest na dwa obszary - 'touchBox' i 'messageBox'.
    'touchBox' wypelniony jest przez 'canvas', a dodatkowo zawiera
    'targetBox' (do nauki 'targetTouches').
    
    Elementy te maja odpowiednie style CSS3, ktore pozwalaja im dopasowac
    sie do ekranu (np. przy zmianie orientacji na i-Padzie) w ten sposob,
    zeby stosunki ich wysokosci oraz szerokosci nie ulegaly zmianie.

--- js/View.class.js
+++ js/View.class.js
@@ -2,37 +2,18 @@ View = function() {
 
     this.init = function(){
 
-        var h = window.innerHeight;
-        var w = window.innerWidth;
-        this.window.resize(h,w);
         this.mainLayout();
 
     };
 
     this.mainLayout = function(){
 
-        var x = this.window.size()[0];
-        var y = this.window.size()[1];
+        $('body').empty().append('<div id="mainDiv"><div id="touchBox"><canvas></canvas><div id="targetBox"></div></div><div id="messageBox"></div></div>');
 
-        $('body').empty().append('<div id="mainDiv"></div>');
-        $('#mainDiv').append(x+' '+y);
-
-    };
-
-    this.window = { height: 0,
-                    width: 0,
-                    resize: function(h,w){
-                        this.height = h;
-                        this.width = w;
-                    },
-                    size: function(){
-                        return [this.height, this.width];
-                    }
     };
 
     this.resizeLayout = function(h,w) {
-        this.window.resize(h,w);
-        this.mainLayout();
+        //this.mainLayout();
     };
 
     this.init();
